a90eb32371e84caf4c6ef4e04c11841a8cdbdacd
AvigailGB <106257812+AvigailGB@users.noreply.github.com>
2022-06-17 13:20:13 +0200

    last commit day 3

--- js/services/gallery-service.js
+++ js/services/gallery-service.js
@@ -1,8 +1,24 @@
 'use strict'
 
 var gImgs = [
-    {id: makeId(), url: 'img/img-meme/1.jpg', keywords: ['Tramp', 'say']},
-    {id: makeId(), url: 'img/img-meme/2.jpg', keywords: ['dogs', 'love']},
+    {id: makeId(), url: 'img/img-meme/1.jpg', keywords: ['Tramp', 'Say']},
+    {id: makeId(), url: 'img/img-meme/2.jpg', keywords: ['Dogs', 'Love']},
+    {id: makeId(), url: 'img/img-meme/3.jpg', keywords: ['Dogs', 'Baby','Sleep']},
+    {id: makeId(), url: 'img/img-meme/4.jpg', keywords: ['Cat', 'Sleep']},
+    {id: makeId(), url: 'img/img-meme/5.jpg', keywords: ['Baby', 'Pawer','Beach']},
+    {id: makeId(), url: 'img/img-meme/6.jpg', keywords: ['Idea']},
+    {id: makeId(), url: 'img/img-meme/7.jpg', keywords: ['Baby', 'Surprise']},
+    {id: makeId(), url: 'img/img-meme/8.jpg', keywords: ['Funny', 'Clown']},
+    {id: makeId(), url: 'img/img-meme/9.jpg', keywords: ['Babby', 'Funny']},
+    {id: makeId(), url: 'img/img-meme/10.jpg', keywords: ['Funny']},
+    {id: makeId(), url: 'img/img-meme/11.jpg', keywords: ['Pawer', 'Funny']},
+    {id: makeId(), url: 'img/img-meme/12.jpg', keywords: ['']},
+    {id: makeId(), url: 'img/img-meme/13.jpg', keywords: ['Cheers', 'Clown']},
+    {id: makeId(), url: 'img/img-meme/14.jpg', keywords: ['Funny', 'Clown']},
+    {id: makeId(), url: 'img/img-meme/15.jpg', keywords: ['Funny', 'Clown']},
+    {id: makeId(), url: 'img/img-meme/16.jpg', keywords: ['Funny', 'Clown']},
+    {id: makeId(), url: 'img/img-meme/17.jpg', keywords: ['Funny', 'Clown']},
+    {id: makeId(), url: 'img/img-meme/18.jpg', keywords: ['Funny', 'Clown']},
 ]
 
 function getImgs(){
